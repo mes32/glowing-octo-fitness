59a6c6776ae68dc8069ccfed042350b43e805ca2
mes32 <stockman.mike@gmail.com>
2020-05-03 16:07:37 -0500

    Setup simple server-side route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 
 const app = express();
+const bodyParser = require('body-parser');
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/* Routes */
+const userRouter = require('./routes/user.router');
+
+app.use('/api/user', userRouter);
 
 // Serve the static site files from build directory
 app.use(express.static('build'));
